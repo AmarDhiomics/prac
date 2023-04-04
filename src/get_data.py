8f3fc242b9123de292e9c9fdfef7341f20e8453a
Amarnath <amarnath.reddy@dhiomics.com>
2023-04-04 11:39:55 +0530

    addedd dvc

--- src/get_data.py
+++ src/get_data.py
@@ -6,7 +6,7 @@ def read_params(config_path):
     with open(config_path) as yaml_file:
         config=yaml.safe_load(yaml_file)
     return config
-
+#testing the repro
 def get_data(config_path):
     config=read_params(config_path)
     data_path=config["data_source"]["s3_source"]
